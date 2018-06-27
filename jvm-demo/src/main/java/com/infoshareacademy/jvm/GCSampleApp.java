fd12bf6894115d67ade9f0c5298910fc3d38af10
Michał Matosek <michal.matosek@wp.pl>
2018-06-27 23:59:17 +0200

    JVM tasks

---
+++ jvm-demo/src/main/java/com/infoshareacademy/jvm/GCSampleApp.java
@@ -0,0 +1,32 @@
+package com.infoshareacademy.jvm;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GCSampleApp {
+
+    public static void main(String[] args) throws IOException {
+        Thread.currentThread().setName("GCSampleThread");
+
+        System.out.println("Press enter to start ...");
+        System.in.read();
+        System.out.println("Starting ...");
+
+        List<Object> list = new ArrayList<>();
+
+        // Keep adding new elements to the 'list'
+        // Clear the list every 10 000 iterations
+        // Monitor the memory in jvisualvm
+
+        int i = 0;
+        while (true) {
+            if (i % 10000 == 0) {
+                list.clear();
+            }
+
+            i++;
+            list.add(new Object());
+        }
+    }
+}
---
+++ jvm-demo/src/main/java/com/infoshareacademy/jvm/OutOfMemorySampleApp.java
@@ -0,0 +1,30 @@
+package com.infoshareacademy.jvm;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OutOfMemorySampleApp {
+
+    public static void main(String[] args) throws IOException {
+        Thread.currentThread().setName("OutOfMemoryThread");
+
+        System.out.println("Press enter to start ...");
+        System.in.read();
+        System.out.println("Starting ...");
+
+        List<Object> list = new ArrayList<>();
+
+        // Keep adding new elements to the 'list'
+        // Print list size every 10 000 iterations
+        // Monitor the memory in jvisualvm
+
+        while (true) {
+            list.add(new Object());
+
+            if (list.size() % 10000 == 0) {
+                System.out.println("List size: " + list.size());
+            }
+        }
+    }
+}
